ca427005194c6633c8facd7103a0b735eb76a43a
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-08 10:52:20 +0600

    add: problem-8 solution

---
+++ src/problem-8.ts
@@ -0,0 +1,9 @@
+{
+  const validateKeys = <T extends object, X extends string|number>(obj: T, keys: X[]): boolean => {
+    for (let key of keys) {
+      if (!(key in obj)) return false;
+    }
+
+    return true;
+  };
+}
